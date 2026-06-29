0d1e3352e02de1b1e89fe91fbcbf75efb28f00e6
Alfred <alfreds.quocho97z@gmail.com>
2026-06-30 02:31:52 +0700

    Force Keystatic admin to dark mode

--- src/app/keystatic/[[...params]]/page.tsx
+++ src/app/keystatic/[[...params]]/page.tsx
@@ -3,4 +3,9 @@
 import { makePage } from '@keystatic/next/ui/app';
 import config from '../../../../keystatic.config';
 
+// Force dark mode before Keystatic reads localStorage to initialize its theme state
+if (typeof window !== 'undefined') {
+  localStorage.setItem('keystatic-color-scheme', 'dark');
+}
+
 export default makePage(config);
